1c87f4ebd9fbdb0a13af5cc129a613be56d3f48c
Gianluca Esposito <g@esposi.to>
2018-03-19 11:56:54 +0100

    Add reactstrap and Bootstrap Italia

--- stories/index.js
+++ stories/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-// storiesOf("Button", module)
-//   .add("", () => ())
+import "bootstrap-italia/dist/css/bootstrap-italia.css";
+
+import { Button } from "reactstrap";
+
+storiesOf("Button", module)
+  .add("", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ));
