c12604e19b1080df2569b9767b52bb85b64ef498
Gianluca Esposito <g@esposi.to>
2018-03-19 17:12:40 +0100

    Add sample Button Group stories

---
+++ src/components/ButtonGroup/ButtonGroup.stories.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+
+import {
+  ButtonToolbar,
+  ButtonGroup,
+  Button
+} from "reactstrap";
+
+import ButtonGroupDropdownExample from "./ButtonGroupDropdownExample";
+
+storiesOf("Componenti/Button Group", module)
+  .addDecorator((story, context) => withInfo("")(story)(context))
+  .add("Esempi", () => (
+    <ButtonGroup>
+      <Button>Left</Button>
+      <Button>Middle</Button>
+      <Button>Right</Button>
+    </ButtonGroup>
+  ))
+  .add("Toolbar", () => (
+    <ButtonToolbar>
+      <ButtonGroup className="mr-1">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>4</Button>
+      </ButtonGroup>
+      <ButtonGroup className="mr-1">
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>7</Button>
+      </ButtonGroup>
+      <ButtonGroup className="mr-1">
+        <Button>8</Button>
+      </ButtonGroup>
+    </ButtonToolbar>
+  ))
+  .add("Dimensioni", () => (
+    <section>
+      <div>
+        <ButtonGroup size="lg">
+          <Button>Left</Button>
+          <Button>Middle</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </div>
+      <div className="mt-3">
+        <ButtonGroup>
+          <Button>Left</Button>
+          <Button>Middle</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </div>
+      <div className="mt-3">
+        <ButtonGroup size="sm">
+          <Button>Left</Button>
+          <Button>Middle</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </div>
+    </section>
+  ))
+  .add("Vertical", () => (
+    <ButtonGroup vertical>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Button>Button</Button>
+    </ButtonGroup>
+  ));
+
+  storiesOf("Componenti/Button Group/Nesting", module)
+    .addDecorator((story, context) => withInfo("")(story)(context))
+    .add("Nesting", () => <ButtonGroupDropdownExample />);
