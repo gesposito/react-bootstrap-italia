ac299fee38748cd35bf9fd1b7030b0fb4e511277
Gianluca Esposito <g@esposi.to>
2018-03-19 13:56:17 +0100

    Add Storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import { setDefaults } from '@storybook/addon-info';
+
+// addon-info
+setDefaults({
+  header: false, // Toggles display of header with component name and description
+  inline: true, // Displays info inline vs click button to view
+  source: true // Displays the source of story Component
+});
+
+const req = require.context("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
