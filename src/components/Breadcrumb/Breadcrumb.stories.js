133ae64fc22293d9279f4d2af79603ca7d379ea2
Gianluca Esposito <g@esposi.to>
2018-03-19 15:38:25 +0100

    Add sample Breadcrumb stories

---
+++ src/components/Breadcrumb/Breadcrumb.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+
+import "bootstrap-italia/dist/css/bootstrap-italia.css";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+
+storiesOf("Componenti/Breadcrumb", module)
+  .addDecorator((story, context) => withInfo("")(story)(context))
+  .add("Esempi", () => (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbItem active>Home</BreadcrumbItem>
+      </Breadcrumb>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <a href="#">Home</a>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>Library</BreadcrumbItem>
+      </Breadcrumb>
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <a href="#">Home</a>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <a href="#">Library</a>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>Data</BreadcrumbItem>
+      </Breadcrumb>
+    </div>
+  ));
