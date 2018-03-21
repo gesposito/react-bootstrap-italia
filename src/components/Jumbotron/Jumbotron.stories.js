061164c04690cb2192cf9bb90f7e80ccc12cb53b
Gianluca Esposito <g@esposi.to>
2018-03-21 10:17:49 +0100

    Add sample Hero stories

---
+++ src/components/Jumbotron/Jumbotron.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+
+import { Jumbotron, Button, Container } from "reactstrap";
+
+storiesOf("Componenti/Hero", module)
+  .addDecorator((story, context) => withInfo("")(story)(context))
+  .add("Esempi", () => (
+    <Jumbotron className="hero w-50">
+      <h1 className="hero-heading display-4">Ciao, mondo!</h1>
+      <p className="lead">
+        Questa è una semplice unità hero, un semplice per richiamare
+        un'attenzione particolare al contenuto o alle informazioni in primo
+        piano.
+      </p>
+      <hr className="my-4" />
+      <p>
+        Utilizza le classi di utilità per la tipografia e la spaziatura per
+        l'ingombro del contenuto all'interno del contenitore più grande.
+      </p>
+      <p className="lead">
+        <Button color="primary">Per saperne di più</Button>
+      </p>
+    </Jumbotron>
+  ))
+  .add("Fluid", () => (
+    <Jumbotron fluid className="hero m-3">
+      <Container fluid>
+        <h1 class="hero-heading display-4">Hero fluido</h1>
+        <p class="lead">
+          Questo è un hero modificato che occupa l'intero spazio orizzontale del
+          suo contenitore.
+        </p>
+      </Container>
+    </Jumbotron>
+  ));
+
