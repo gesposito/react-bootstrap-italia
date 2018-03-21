4ba9d2f525ac51b09c2875c08df056219bdab68b
Gianluca Esposito <g@esposi.to>
2018-03-21 11:12:28 +0100

    Add sample InputGroup stories

---
+++ src/components/InputGroup/InputGroup.stories.js
@@ -0,0 +1,159 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  FormGroup,
+  Label,
+  Button,
+} from "reactstrap";
+
+import InputGroupDropdownExample from "./InputGroupDropdownExample";
+
+storiesOf("Componenti/Input Group", module)
+  .addDecorator((story, context) => withInfo("")(story)(context))
+  .add("Esempi", () => (
+    <div className="p-3">
+      <InputGroup className="pb-3">
+        <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+        <Input type="text" placeholder="Username" />
+      </InputGroup>
+
+      <InputGroup className="pb-3">
+        <Input type="text" placeholder="Mail utente" />
+        <InputGroupAddon addonType="append">@esempio.it</InputGroupAddon>
+      </InputGroup>
+
+      <FormGroup>
+        <InputGroup className="pb-3">
+          <InputGroupAddon addonType="prepend">
+            https://esempio.it/utente/
+          </InputGroupAddon>
+          <Input type="text" id="basic-url" />
+        </InputGroup>
+        <Label for="basic-url">La tua URL</Label>
+      </FormGroup>
+
+      <InputGroup className="pb-3">
+        <InputGroupAddon addonType="prepend">€</InputGroupAddon>
+        <Input type="number" />
+        <InputGroupAddon addonType="append">,00</InputGroupAddon>
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">Textarea</InputGroupAddon>
+        <Input type="textarea" />
+      </InputGroup>
+    </div>
+  ))
+  .add("Dimensioni", () => (
+    <div>
+      <InputGroup size="sm" className="mb-3">
+        <InputGroupAddon addonType="prepend">Testo piccolo</InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroupAddon addonType="prepend">Testo predefinito</InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+
+      <InputGroup size="lg">
+        <InputGroupAddon addonType="prepend">Testo grande</InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+    </div>
+  ))
+  .add("Checkbox e radio", () => (
+    <div>
+      <InputGroup className="mb-3">
+        <InputGroupAddon addonType="prepend">
+          <InputGroup>
+            <Input type="checkbox" />
+          </InputGroup>
+        </InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">
+          <InputGroup>
+            <Input type="radio" />
+          </InputGroup>
+        </InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+    </div>
+  ))
+  .add("Input multipli", () => (
+    <div>
+      <InputGroup>
+        <InputGroupAddon addonType="prepend">Nominativo</InputGroupAddon>
+        <Input type="text" placeholder="Nome" />
+        <Input type="text" placeholder="Cognome" />
+      </InputGroup>
+    </div>
+  ))
+  .add("Aggiunte multiple", () => (
+    <div>
+      <InputGroup className="mb-3">
+        <InputGroupAddon addonType="prepend">
+          <InputGroupText>€</InputGroupText>
+          <InputGroupText>0,00</InputGroupText>
+        </InputGroupAddon>
+        <Input type="text" />
+      </InputGroup>
+
+      <InputGroup>
+        <Input type="text" />
+        <InputGroupAddon addonType="append">
+          <InputGroupText>€</InputGroupText>
+          <InputGroupText>0,00</InputGroupText>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  ))
+  .add("Aggiunta bottoni", () => (
+    <div>
+      <InputGroup className="mb-3">
+        <InputGroupAddon addonType="prepend">
+          <Button color="primary">Bottone</Button>
+        </InputGroupAddon>
+        <Input type="text" placeholder="" />
+      </InputGroup>
+
+      <InputGroup className="input-group mb-3">
+        <Input type="text" placeholder="Contenitore" />
+        <InputGroupAddon addonType="append">
+          <Button color="primary">Bottone</Button>
+        </InputGroupAddon>
+      </InputGroup>
+
+      <InputGroup className="input-group mb-3">
+        <InputGroupAddon addonType="prepend">
+          <Button color="primary">Bottone</Button>
+          <Button color="primary">Bottone</Button>
+        </InputGroupAddon>
+        <Input type="text" placeholder="" />
+      </InputGroup>
+
+      <InputGroup className="input-group">
+        <Input type="text" placeholder="Contenitore" />
+        <InputGroupAddon addonType="append">
+          <Button color="primary">Bottone</Button>
+          <Button color="primary">Bottone</Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  ))
+  .add("Bottoni con dropdown", () => (
+    <InputGroupDropdownExample />
+  ))
+  .add("Pulsanti segmentati", () => (
+    <InputGroupDropdownExample split />
+  ));
---
+++ src/components/InputGroup/InputGroupDropdownExample.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButtonDropdown,
+  Input,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button
+} from "reactstrap";
+
+class InputGroupDropdownExample extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false,
+      splitButtonOpen: false,
+    };
+
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+    this.toggleSplit = this.toggleSplit.bind(this);
+  }
+
+  toggleDropDown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  toggleSplit() {
+    this.setState({
+      splitButtonOpen: !this.state.splitButtonOpen
+    });
+  }
+
+  render() {
+    const { split } = this.props;
+    return <div>
+        <InputGroup className="mb-3">
+          <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+            <DropdownToggle caret color="primary">
+              Dropdown
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+          <Input type="text" />
+        </InputGroup>
+
+        <InputGroup>
+          <Input type="text" />
+          <InputGroupButtonDropdown addonType="append" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
+            {(() => {
+              if (split) {
+                return <div>
+                    <Button color="primary">
+                      Action
+                    </Button>
+                    <DropdownToggle split color="primary" />
+                  </div>;
+              }
+
+              return <DropdownToggle caret color="primary">
+                  Dropdown
+                </DropdownToggle>;
+            })()}
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+        </InputGroup>
+      </div>;
+  }
+}
+
+export default InputGroupDropdownExample;
