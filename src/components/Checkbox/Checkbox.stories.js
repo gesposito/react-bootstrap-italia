73fe24e7ebc5247715a5eebacf1c6b46175540d3
Gianluca Esposito <g@esposi.to>
2018-03-30 15:39:44 +0200

    Update Checkbox stories

--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -3,16 +3,31 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { action } from "@storybook/addon-actions";
 
-import { Form, FormGroup, Label, Input, FormText, Row, Col } from "reactstrap";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+
+import CheckboxGroupsExample from "./CheckboxGroupsExample";
 
 storiesOf("Componenti/Checkbox", module)
   .addDecorator((story, context) => withInfo("")(story)(context))
   .add("Esempi", () => (
     <Form>
       <FormGroup check>
-        <input id="checkbox1" type="checkbox" />
+        <Input id="checkbox1" type="checkbox" />
         <Label for="checkbox1" check>
           Label di esempio
+        </Label>
+      </FormGroup>
+      <FormGroup check>
+        <Input name="gruppo1" type="radio" id="radio1" defaultChecked />
+        <Label check for="radio1">
+          Opzione 1
+        </Label>
+      </FormGroup>
+
+      <FormGroup check>
+        <Input name="gruppo1" type="radio" id="radio2" />
+        <Label check for="radio2">
+          Opzione 2
         </Label>
       </FormGroup>
     </Form>
@@ -20,13 +35,13 @@ storiesOf("Componenti/Checkbox", module)
   .add("Inline", () => (
     <Form>
       <FormGroup check inline>
-        <input id="checkbox1" type="checkbox" />
+        <Input id="checkbox1" type="checkbox" />
         <Label for="checkbox1" check>
           Checkbox non selezionato
         </Label>
       </FormGroup>
       <FormGroup check inline>
-        <input id="checkbox2" type="checkbox" defaultChecked />
+        <Input id="checkbox2" type="checkbox" defaultChecked />
         <Label for="checkbox2" check>
           Checkbox selezionato
         </Label>
@@ -36,77 +51,17 @@ storiesOf("Componenti/Checkbox", module)
   .add("Disabilitate", () => (
     <Form>
       <FormGroup check>
-        <input id="checkbox1" type="checkbox" disabled />
+        <Input id="checkbox1" type="checkbox" disabled />
         <Label for="checkbox1" check>
           Checkbox disabilitato non selezionato
         </Label>
       </FormGroup>
       <FormGroup check>
-        <input id="checkbox2" type="checkbox" disabled defaultChecked />
+        <Input id="checkbox2" type="checkbox" disabled defaultChecked />
         <Label for="checkbox2" check>
           Checkbox disabilitato selezionato
         </Label>
       </FormGroup>
     </Form>
   ))
-  .add("Gruppi di Checkbox", () => (
-    <Row className="m-5">
-      <Col md={5}>
-        <Form>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox1" type="checkbox" defaultChecked />
-            <Label for="checkbox1" check>
-              Checkbox selezionato
-            </Label>
-          </FormGroup>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox2" type="checkbox" />
-            <Label for="checkbox2" check>
-              Checkbox non selezionato
-            </Label>
-          </FormGroup>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox3" type="checkbox" disabled />
-            <Label for="checkbox3" check>
-              Checkbox disabilitato non selezionato
-            </Label>
-          </FormGroup>
-        </Form>
-      </Col>
-      <Col md={2} />
-      <Col md={5}>
-        <Form>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox1" type="checkbox" defaultChecked />
-            <Label for="checkbox1" check>
-              Checkbox selezionato
-            </Label>
-            <FormText color="muted">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              molestie libero
-            </FormText>
-          </FormGroup>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox2" type="checkbox" />
-            <Label for="checkbox2" check>
-              Checkbox non selezionato
-            </Label>
-            <FormText color="muted">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              molestie libero
-            </FormText>
-          </FormGroup>
-          <FormGroup check className="form-check-group">
-            <input id="checkbox2" type="checkbox" disabled />
-            <Label for="checkbox2" check>
-              Checkbox disabilitato non selezionato
-            </Label>
-            <FormText color="muted">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              molestie libero
-            </FormText>
-          </FormGroup>
-        </Form>
-      </Col>
-    </Row>
-  ));
+  .add("Gruppi di Checkbox", () => <CheckboxGroupsExample />);
